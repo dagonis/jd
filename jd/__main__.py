11280f850c86c37c17250e64751de604a4969786
Kevin Tyers <dagonis@github.com>
2024-02-29 22:07:13 -0700

    Significant Updates

--- jd/__main__.py
+++ jd/__main__.py
@@ -9,24 +9,32 @@ def main() -> None:
     parser.add_argument("--jd_root", 
                         help="The root of the Johnny Decimal library. This is can also be set with the environment variable JD_ROOT", 
                         default=os.getenv("JD_ROOT", "~/jd"))
+    parser.add_argument("--dry_run", help="Don't actually do anything, just print what would happen", action="store_true")
     subparsers = parser.add_subparsers(help="Johnny Decimal command help")
     # Subparser for searching JD
     search_parser = subparsers.add_parser("search", help="Search Johnny Decimal")
     search_parser.add_argument("search_term", help="The search term to use")
-    # search_parser.add_parser("search_term", help="The search term to use")
+    search_parser.add_argument("--include_category", "-c", help="Include the category in the search results", action="store_true")
+    search_parser.add_argument("--include_files", "-f", help="Include the files in the search results", action="store_true")
+    # Subparser for adding JD Categories
+    add_category_parser = subparsers.add_parser("add_category", help="Add a new Johnny Decimal category")
+    add_category_parser.add_argument("area_id", help="The area ID to add the category to")
+    add_category_parser.add_argument("new_category_name", help="The name of the new category to add")
+    # Subparser for adding JD Identifiers
+    add_identifier_parser = subparsers.add_parser("add_id", help="Add a new Johnny Decimal identifier")
+    add_identifier_parser.add_argument("category_id", help="The category ID to add the identifier to")
+    add_identifier_parser.add_argument("new_identifier_name", help="The name of the new identifier to add")
+    # On to the rest of the script
     args = parser.parse_args()
-    print(args)
     jd = JohnDecimal(args.jd_root)
     if hasattr(args, 'search_term'):
-        print(jd.search_johnny_decimal_category(args.search_term))
+        print(jd.search_johnny_decimal(args.search_term, args.include_category, args.include_files))
+    elif hasattr(args, 'new_category_name'):
+        jd.add_johnny_decimal_category(args.area_id, args.new_category_name, args.dry_run)
+    elif hasattr(args, 'new_identifier_name'):
+        jd.add_johnny_decimal_identifier(args.category_id, args.new_identifier_name, args.dry_run)
     else:
-        print(jd)
-    print("Areas")
-    for area in jd.areas:
-        print(area.name)
-    print("Categories")
-    for category in jd.categories:
-        print(category.name)
+        print(jd.print_johnny_decimal_tree())
 
 
 if __name__ == '__main__':
--- jd/core.py
+++ jd/core.py
@@ -7,95 +7,172 @@ from pathlib import Path
 
 @dataclass
 class JohnDecimal:
+    """
+    This represents the whole of a Johnny Decimal library. 
+    All you need to give it is a file system location and it will build the whole library.
+    All the fun methods related to searching and adding Johnny Decimals are here.
+
+    The implied format of a JD ID is AC.ID where AC is area/category and ID is the identifier.
+    """
     file_system_location:str
 
-    def __post_init__(self):
-        self.areas = sorted([Area(_) for _ in Path(self.file_system_location).iterdir() if _.is_dir() and _.name[0] != "."], key=lambda _: _.name)
-        # self.areas = list(chain(*[_.areas for _ in self.area_groups]))
-        # self.categories = list(chain(*[_.categories for _ in self.areas]))
+    def __post_init__(self) -> None:
+        self.areas = sorted([Area(_) for _ in Path(self.file_system_location).iterdir() if _.is_dir() and _.name[0] != "."], key=lambda _: _.area_name)
+        self.categories = list(chain(*[area.categories for area in self.areas]))
+        self.identifiers = list(chain(*[category.identifiers for category in self.categories]))
+        self.files = list(chain(*[identifier.files for identifier in self.identifiers]))
 
-    def search_johnny_decimal_category(self, search_term:str) -> str:
+    """ 
+    These Methods are for searching and printing the Johnny Decimal library.
+    Think of this as the "read" part of the CRUD operations.
+    """
+
+    def search_johnny_decimal(self, search_term:str, include_category=True, include_files=False) -> str:
         results = []
-        for area_group in self.area_groups:
-            for area in area_group.areas:
-                for category in area.categories:
-                    if search_term in category.name:
-                        print(f"Found {category.name} in {area.name}")
-                        _result = f"{area.number}.{category.number} - {category.file_system_location}"
-                        results.append(_result)
-        output_string = ""
-        for result in results:
-            output_string = output_string + f"{result}\n"
-        return output_string.rstrip()
+        normalized_search_term = search_term.lower()
+        for category in self.categories:
+            if normalized_search_term in category.category_name.lower():
+                results.append(str(category))
+            for identifier in category.identifiers:
+                if normalized_search_term in identifier.full_name.lower():
+                    if include_category:
+                        results.append(f"{category} -> {identifier}")
+                    else:
+                        results.append(str(identifier))
+                if include_files:
+                    for jdfile in identifier.files:
+                        if normalized_search_term in jdfile.file_system_location.name.lower():
+                            results.append(str(jdfile))
+        if len(results) == 0:
+            return f"No results found for {search_term}"
+        return "\n".join(results)
     
-    def search_johnny_decimal_identifier(self, search_term:str) -> str:
-        results = []
-        for area_group in self.area_groups:
-            for area in area_group.areas:
-                for category in area.categories:
-                    for identifier in category.identifiers:
-                        if search_term in identifier.name:
-                            _result = f"{area.number}{category.number}{identifier.name} - {identifier.file_system_location}"
-                            results.append([identifier])
-        return results
-
-    def add_johnny_decimal(self, jd:str) -> str:
-        pass
+    def print_johnny_decimal_tree(self, space_len = 4, tabs = False, print_files = False) -> str:
+        output = ""
+        spaces = " " * space_len if not tabs else "\t"
+        for area in self.areas:
+            output += f"{area}\n"
+            for category in area.categories:
+                output += f"{spaces}{category}\n"
+                for identifier in category.identifiers:
+                    output += f"{spaces}{spaces}{identifier}\n"
+                    if print_files:
+                        for jdfile in identifier.files:
+                            output += f"{spaces}{spaces}{spaces}{jdfile}\n"
+                    else:
+                        pass
+        return output
 
     def __str__(self) -> str:
-        output_string = ""
+        return str(self.identifiers)    
+
+    """
+    These methods are for adding Johnny Decimal items.
+    Think of this as the "create" part of the CRUD operations.
+    """
+
+    def add_johnny_decimal_category(self, area_id:str, new_category_name:str, dry_run=False) -> bool:
         for area in self.areas:
-            output_string = output_string + f"{area_group.name}\n"
-            for category in area_group.areas:
-                output_string = output_string + f"    {area.name}\n"
-                for category in area.categories:
-                    output_string = output_string + f"        {category.name}\n"
-                    for identifier in category.identifiers:
-                        output_string = output_string + f"            {identifier.name}\n"
-        return output_string
+            if area.area_number_range.startswith(area_id):
+                for i in range(int(area_id), int(area_id) + 10):
+                    if not any(category.category_number == str(i).zfill(2) for category in area.categories):
+                        _new_category = f"{i} {new_category_name}"
+                        if not dry_run:
+                            os.mkdir(Path(area.file_system_location) / _new_category)
+                            return True
+                        print(f"Would have created - {area.file_system_location}/{_new_category}")
+                        return False
+                    
+    def add_johnny_decimal_identifier(self, category_id:str, identifier_name:str, dry_run=False) -> bool:
+        for category in self.categories:
+            if category.category_number == category_id:
+                for i in range(1, 100):
+                    if not any(identifier.id_number == str(i).zfill(2) for identifier in category.identifiers):
+                        new_identifier = f"{str(i).zfill(2)} {identifier_name}"
+                        if not dry_run:
+                            os.mkdir(Path(category.file_system_location) / new_identifier)
+                            return True
+                        print(f"Would have created - {category.file_system_location}/{new_identifier}")
+                        return False
+
+
+
+
 
 @dataclass
 class Area:
+    """
+    This represents a Johnny Decimal area.
+    This is usually created by the JohnDecimal class.
+    This is in the format of: 00-09 Admin or 80-89 Unused
+    It is somewhat unlikely you will ever deal with this yourself, you are probably more interested in
+    categories and identifiers.
+    """
     file_system_location:str
 
-    def __post_init__(self):
-        self.name = self.file_system_location.parts[-1]
-        self.areas = sorted([Area(_) for _ in Path(self.file_system_location).iterdir() if _.is_dir() and _.name[0] != "."], key=lambda _: _.name)
+    def __post_init__(self) -> None:
+        self.area_name = self.file_system_location.parts[-1]
+        self.area_number_range, self.area_short_name = self.area_name.split(" ", maxsplit=1)
+        self.categories = sorted([Category(_, self.area_name) for _ in Path(self.file_system_location).iterdir() if _.is_dir() and _.name[0] != "."], key=lambda _: _.category_name)
 
-# @dataclass
-# class Category:
-#     file_system_location:str
-
-#     def __post_init__(self):
-#         self.name = self.file_system_location.parts[-1]    
-#         self.number = self.name.split(" ")[0]
-#         self.categories = sorted([Category(_, self.number) for _ in Path(self.file_system_location).iterdir() if _.is_dir() and _.name[0] != "."], key=lambda _: _.name)
-
+    def __str__(self) -> str:
+        return f"[{self.area_number_range}] {self.area_short_name}"
 
 @dataclass
 class Category:
+    """
+    This represents a Johnny Decimal category.
+    This is usually created by the Area class.
+    This will be the most common class you will deal with.
+    The format is Category and will look like ##
+    In a whole ID, it would be like Category.Identifier e.g. 11.01
+    """
     file_system_location:str
     area:str
 
     def __post_init__(self):
-        self.name = self.file_system_location.parts[-1]
-        self.number = self.name.split(" ")[0]
-        self.identifiers = sorted([Identifier(_, self.number) for _ in Path(self.file_system_location).iterdir() if _.is_file() and _.name[0] != "."], key=lambda _: _.name)
+        self.category_name = self.file_system_location.parts[-1]    
+        self.category_number, self.category_short_name = self.category_name.split(" ", maxsplit=1)
+        self.identifiers = sorted([Identifier(_, self.area, self.category_number) for _ in Path(self.file_system_location).iterdir() if _.is_dir() and _.name[0] != "."], key=lambda _: _.full_name)
 
+    def __str__(self) -> str:
+        return f"[{self.category_number}] {self.category_short_name}"
 
 @dataclass
 class Identifier:
+    """
+    This represents a Johnny Decimal identifier.
+    This is usually created by the Category class.
+    This is the most part of the JD structure and contains files.
+    The format is Identifier and will look like ##
+    In a whole ID, it would be like Category.Identifier e.g. 11.01
+    """
     file_system_location:str
+    area:str
     category:str
 
-    def __post_init__(self):
-        self.name = self.file_system_location.parts[-1]
-        self.number = self.name.split(" ")[0]
-        self.contents = self.list_files()
+    def __post_init__(self) -> None:
+        self.full_name = self.file_system_location.parts[-1]
+        self.id_number, self.short_name = self.full_name.split(" ", maxsplit=1)
+        self.files = [JohnnyDecimalFile(_, self.area, self.category, self.id_number) for _ in Path(self.file_system_location).iterdir() if _.is_file() and _.name[0] != "."]
+    
+    def __str__(self) -> str:
+        return f"[{self.category}.{self.id_number}] {self.short_name}"
+    
+@dataclass
+class JohnnyDecimalFile:
+    """
+    This represents a file in a Johnny Decimal identifier.
+    This is usually created by the Identifier class.
+    """
+    file_system_location:str
+    area:str
+    category:str
+    identifier:str
 
-    def list_files(self):
-        file_list = []
-        for root, dirs, files in os.walk(self.file_system_location):
-            for f in files:
-                file_list.append(os.path.join(root, f))
-        return file_list
+    def __post_init__(self) -> None:
+        self.file_name = self.file_system_location.parts[-1]
+
+    def __str__(self) -> str:
+        return f"[{self.category}.{self.identifier}] {self.file_name}"
+    
